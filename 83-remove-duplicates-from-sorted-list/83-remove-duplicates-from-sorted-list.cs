6a1766885ae7e74446170681153e0b290a9101d3
Manju Bhagtani <31012554+ManjuBhagtani@users.noreply.github.com>
2022-03-05 16:25:15 +0530

    Time: 120 ms (52.38%), Space: 38.1 MB (60.05%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cs
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cs
@@ -11,21 +11,25 @@
  */
 public class Solution {
     public ListNode DeleteDuplicates(ListNode head) {
+             
+        if(head == null){
+            return head;
+        }
+        
         ListNode temp = head;
         
-        while(temp != null){
-            if(temp.next != null){
-                if(temp.val == temp.next.val){
-                    temp.next = temp.next.next;                   
-                }else{
-                    temp = temp.next;
-                }
+        while(temp.next != null){
+            if(temp.val == temp.next.val){
+                temp.next = temp.next.next;
             }else{
                 temp = temp.next;
             }
-            
         }
-        
         return head;
     }
 }
+
+/*
+    TC = O(N)
+    SC = O(1)
+*/
