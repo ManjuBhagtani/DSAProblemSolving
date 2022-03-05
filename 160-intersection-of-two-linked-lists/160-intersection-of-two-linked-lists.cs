ff5e74f13a54ec22941444f80429a6a1a6f268a0
Manju Bhagtani <31012554+ManjuBhagtani@users.noreply.github.com>
2022-03-05 20:08:18 +0530

    Time: 160 ms (61.39%), Space: 45.9 MB (38.88%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cs
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cs
@@ -8,29 +8,47 @@
  */
 public class Solution {
     public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode temp1 = headA;
+        //Approach 1 - Using Hashset
+        // ListNode temp1 = headA;
         
-        HashSet<ListNode> hs = new HashSet<ListNode>();
+        // HashSet<ListNode> hs = new HashSet<ListNode>();
         
-        while(temp1 != null){
-            hs.Add(temp1);
-            temp1 = temp1.next;
+        // while(temp1 != null){
+        //     hs.Add(temp1);
+        //     temp1 = temp1.next;
+        // }
+        
+        // ListNode temp2 = headB;
+        
+        // while(temp2 != null){
+        //     if(hs.Contains(temp2)){
+        //         return temp2;
+        //     }
+        //     temp2 = temp2.next;
+        // }
+        
+        // return null;
+        
+        /*
+            TC = O(list1 length + list2 length)
+            SC = O(list1 length)
+        */
+        
+        //Approach 2
+        ListNode a = headA;
+        ListNode b = headB;
+        
+        while(a != b){
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
         
-        ListNode temp2 = headB;
+        return a; //or b
         
-        while(temp2 != null){
-            if(hs.Contains(temp2)){
-                return temp2;
-            }
-            temp2 = temp2.next;
-        }
-        
-        return null;
+        /*
+            TC = O(list1 length + list2 length)
+            SC = O(1)
+        */
     }
 }
 
-/*
-    TC = O(list1 length + list2 length)
-    SC = O(list1 length)
-*/
