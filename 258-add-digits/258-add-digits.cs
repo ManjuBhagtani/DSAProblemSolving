225e2bd5638a38a6ca2f663ca2b2e10bf87d0862
Manju Bhagtani <31012554+ManjuBhagtani@users.noreply.github.com>
2022-03-08 10:56:33 +0530

    Time: 43 ms (25.12%), Space: 25.3 MB (63.23%) - LeetHub

--- 258-add-digits/258-add-digits.cs
+++ 258-add-digits/258-add-digits.cs
@@ -1,18 +1,27 @@
 public class Solution {
     public int AddDigits(int num) {
-        int sum = 0;
-        while(num > 0){
-            int rem = num%10;
-            sum += rem;
-            num /= 10;
-            if(num == 0){
-                if(sum >= 10){
-                    num = sum;
-                    sum = 0;
-                }
-            }
-        }
+        // int sum = 0;
+        // while(num > 0){
+        //     int rem = num%10;
+        //     sum += rem;
+        //     num /= 10;
+        //     if(num == 0){
+        //         if(sum >= 10){
+        //             num = sum;
+        //             sum = 0;
+        //         }
+        //     }
+        // }
         
-        return sum;
+        // return sum;
+        
+        //O(1) solution below:
+        if(num == 0){
+            return 0;
+        }
+        if(num%9 == 0){
+            return 9;
+        }
+        return num%9;
     }
 }
