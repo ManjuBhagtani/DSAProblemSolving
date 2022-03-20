878a3a86148ccc5171519ffb2a66cb18fa342450
Manju Bhagtani <31012554+ManjuBhagtani@users.noreply.github.com>
2022-03-20 18:15:19 +0530

    Time: 124 ms (98.57%), Space: 46 MB (19.11%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        //Rows and columns are sorted in increasing order
+        int n = matrix.Length;
+        int m = matrix[0].Length; 
+        int i = 0;
+        int j = m-1; //start from top right corner
+        
+        while(i<n && j>=0){ 
+            if(matrix[i][j] == target){ //if top right corner == target
+                return true;
+            }
+            if(matrix[i][j] > target){ //top right corner value > target. So all elements below current element > target. So, eliminate the column.
+                j--;
+            }else{ //top right corner value < target. So current row cannot have the target element. So, eliminate the row.
+                i++;
+            }
+        }
+        
+        return false;
+        
+        /*
+            TC = O(n+m)
+            SC = O(1)
+        */
+    }
+}
