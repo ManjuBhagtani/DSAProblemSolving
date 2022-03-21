3661be2e14fea4f6d0752706c9697a1b6a673dad
Manju Bhagtani <31012554+ManjuBhagtani@users.noreply.github.com>
2022-03-21 09:30:53 +0530

    Time: 152 ms (37.57%), Space: 40.2 MB (55.49%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cs
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cs
@@ -1,21 +1,57 @@
 public class Solution {
     public int CountNegatives(int[][] grid) {
+//         int count = 0;
+//         int n = grid.Length;
+//         int m = grid[0].Length;
+        
+//         int i = 0;
+//         int j = m-1; //start from top right corner
+        
+//         while(i < n && j >= 0){
+//             if(grid[i][j] < 0){
+//                 count += n-i; //all elements in this column starting from current row are -ve
+//                 j--;
+//             }else{
+//                 i++;
+//             }
+//         }
+        
+//         return count;
+        
+        /*
+            TC = O(n+m)
+            SC = O(1)
+        */
+        
+        /*-----------Approach 2 - Binary Search----------------------*/
+        int m = grid.Length;
+        int n = grid[0].Length;
         int count = 0;
-        int n = grid.Length;
-        int m = grid[0].Length;
-        
-        int i = 0;
-        int j = m-1; //start from top right corner
-        
-        while(i < n && j >= 0){
-            if(grid[i][j] < 0){
-                count += n-i; //all elements in this column starting from current row are -ve
-                j--;
-            }else{
-                i++;
+        for(int i = 0; i<m; i++){
+            int start = 0;
+            int end = n-1;
+            int ans = n;
+            
+            while(start <= end){
+                int mid = (end-start)/2 + start;
+                if(grid[i][mid] < 0){
+                    ans = mid;
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
             }
+            
+            count += n-ans;
         }
         
         return count;
+        
+        /*
+            TC = O(mlogn) - we are applying binary search(logn) on each row(m rows)
+            SC = O(1)
+            Binary search approach doesn't take advantage of sortedness of columns
+            If the no. of rows(m) is huge, then mlogn > n+m
+        */
     }
 }
