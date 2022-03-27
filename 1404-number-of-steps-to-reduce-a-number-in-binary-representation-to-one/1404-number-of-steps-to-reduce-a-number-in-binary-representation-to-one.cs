6912b5a3488139c01af52c190642a88a234e258f
Manju Bhagtani <31012554+ManjuBhagtani@users.noreply.github.com>
2022-03-27 17:18:11 +0530

    Time: 60 ms (88.89%), Space: 36.1 MB (37.04%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cs
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cs
@@ -1,31 +1,28 @@
 public class Solution {
     public int NumSteps(string s) {
-        StringBuilder sb = new StringBuilder(s);
-        
+              
         int steps = 0;
         
-        while(sb.ToString() != "1"){
-            int n = sb.Length;
-            if(sb[n-1] == '1'){ //odd number
-                for(int i = n-1; i>=0; i--){
-                    if(sb[i] == '1'){
-                        sb[i] = '0';
-                    }else{
-                        sb[i] = '1';
-                        break;
-                    }
-                }
-                if(sb[0] != '1'){
-                    sb.Insert(0, '1');
-                }
-            }else{ //even no.
-                sb.Remove(n-1, 1);
+        int n = s.Length;
+        
+        int carry = 0;
+        
+        for(int i = n-1; i>=1; i--){
+            int rightmostBit = s[i]-'0';
+            
+            if(rightmostBit+carry == 1){ //odd
+                carry = 1;
+                steps += 2;
+            }else{
+                steps++;
             }
-            steps++;
         }
         
-        return steps;
+        return steps+carry;
         
-        
+        /*
+            TC = O(n)
+            SC = O(1)
+        */
     }
 }
