a19f92b961281595751132d6874e014d512852bc
Manju Bhagtani <31012554+ManjuBhagtani@users.noreply.github.com>
2022-05-02 13:02:27 +0530

    Time: 207 ms (30.63%), Space: 42.3 MB (29.73%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cs
@@ -0,0 +1,48 @@
+public class Solution {
+    public IList<int> TargetIndices(int[] nums, int target) {
+        Array.Sort(nums);
+        int n = nums.Length;
+        int low = 0;
+        int high = n-1;
+        int firstOccurrence = -1;
+        int lastOccurrence = -1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                firstOccurrence = mid;
+                high = mid-1;
+            }else if(nums[mid] < target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        if(firstOccurrence != -1){
+            lastOccurrence = firstOccurrence;
+            low = lastOccurrence+1;
+            high = n-1;
+            while(low <= high){
+                int mid = low + (high-low)/2;
+                if(nums[mid] == target){
+                    lastOccurrence = mid;
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        List<int> ans = new List<int>();
+        if(firstOccurrence > -1){
+            for(int i = firstOccurrence; i<=lastOccurrence; i++){
+                ans.Add(i);
+            }
+        }
+        
+        return ans;
+        
+        /*
+            TC = O(logn)
+            SC = O(1)
+        */
+    }
+}
