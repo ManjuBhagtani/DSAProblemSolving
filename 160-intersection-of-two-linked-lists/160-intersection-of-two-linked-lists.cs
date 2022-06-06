fb7f63ab13fdc4ae3cc4f516c5628641beb23387
Manju Bhagtani <31012554+ManjuBhagtani@users.noreply.github.com>
2022-06-06 09:47:16 +0530

    Time: 138 ms (68.93%), Space: 45.7 MB (72.22%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cs
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cs
@@ -35,20 +35,61 @@ public class Solution {
         */
         
         //Approach 2
-        ListNode a = headA;
-        ListNode b = headB;
+        //ListNode a = headA;
+        //ListNode b = headB;
         
-        while(a != b){
-            a = a == null ? headB : a.next;
-            b = b == null ? headA : b.next;
-        }
+        //while(a != b){
+        //   a = a == null ? headB : a.next;
+        //  b = b == null ? headA : b.next;
+        //}
         
-        return a; //or b
+        //return a; //or b
         
         /*
             TC = O(list1 length + list2 length)
             SC = O(1)
         */
+        
+        //Approach 3
+        int n = 0;
+        ListNode t1 = headA;
+        while(t1 != null){
+            n++;
+            t1 = t1.next;
+        }
+        
+        int m = 0;
+        ListNode t2 = headB;
+        while(t2 != null){
+            m++;
+            t2 = t2.next;
+        }
+        
+        int diff = Math.Abs(n-m);
+        
+        //assume m is greater
+        t1 = headB;
+        t2 = headA;
+        if(n > m){ //n is greater
+            t1 = headA;
+            t2 = headB;
+        }
+        
+        int x = 0;
+        while(x < diff){
+            x++;
+            t1 = t1.next;
+        }
+        while(t1 != t2){
+            t1 = t1.next;
+            t2 = t2.next;
+        }
+        
+        if(t1 == null || t2 == null){
+            return null;
+        }
+        return t1;
+        
     }
 }
 
